43c5ac9b8cc2e573f58316a286e9dcdf2b839502
Wesbert Edouard <wesbert@mail.usf.edu>
2021-01-25 02:49:15 -0500

    Updated urls.py

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,8 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('app.urls')),
+    path('', include('app.urls')),
 ]
